6227303b033b9c6daf189dafa9b554e82db50567
Taisiya Tomashevska <taya22011985@gmail.com>
2023-03-31 22:23:58 +0300

    add prop

--- src/components/MovieList/MoviesList.jsx
+++ src/components/MovieList/MoviesList.jsx
@@ -1,6 +1,8 @@
 import { Link, useLocation } from 'react-router-dom';
+import PropTypes from 'prop-types';
 // import { nanoid } from 'nanoid';
 import css from './MovieList.module.css';
+
 const MoviesList = ({ movies }) => {
   const location = useLocation();
   return (
@@ -20,3 +22,12 @@ const MoviesList = ({ movies }) => {
   );
 };
 export default MoviesList;
+
+MoviesList.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      name: PropTypes.string,
+      title: PropTypes.string,
+    }))
+}
